dd4c2364561fa01fddd3f4eb5729972abbb5b88f
amradelata <amradelata@gmail.com>
2020-03-08 23:23:14 +0200

    phone

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
 
-let menuToggler = document.querySelector('.menu');
-let menu = document.querySelector('.links-phone');
-
-        menuToggler.addEventListener('click', function () {
-            menu.classList.toggle('open');
-        })
