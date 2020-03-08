2d0305827e237bae7ad6a816d533b0cc48ff3fb3
amradelata <amradelata@gmail.com>
2020-03-08 10:16:55 +0200

    3kod

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+let menuToggler = document.querySelector('.menu');
+let menu = document.querySelector('.links-phone');
+
+        menuToggler.addEventListener('click', function () {
+            menu.classList.toggle('open');
+        })
